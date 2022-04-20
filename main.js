dab72439d8faaede602c963eee480c684b7d8444
mammadmammadli <memmed199@gmail.com>
2022-04-20 16:45:14 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,104 @@
+// var a = "Hello";
+// a = "Hello p127";
+
+// const constantString = "Ali";
+
+// let letString = "let";
+
+// letString = "Test";
+
+// console.log(letString);
+
+// const myNumber = 10;
+// const myString = "10";
+// const myBoolean = true;
+// const myFloat = 10.5;
+// const myNull = null;
+// const myUndefined = undefined;
+
+// const user = {
+//   name: "Evez",
+//   surname: "Mammadli",
+//   age: 25,
+//   interests: ["Javascript", "Prius"],
+//   call: function () {
+//     console.log("Salam " + this.name);
+//   },
+// };
+
+// user.name = "Mammad";
+
+// error verecek
+// user = {
+//   name: "Mammad",
+// };
+
+// const myArray = [user, user, user, user, user];
+
+// console.table(myArray);
+
+// if ("10" === 10) {
+//   console.log("worked");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// while
+// let iteration = 0;
+
+// while (iteration < 10) {
+//   iteration++;
+
+//   console.log(iteration + "while");
+// }
+
+// switch case
+// const day = "Friday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("today is the monday");
+
+//     break;
+//   case "Wednesday":
+//     document.write("today is the wed");
+
+//     break;
+
+//   default:
+//     document.write("Bele bir gun yoxdur");
+//     break;
+// }
+
+// try {
+//   console.log(abc);
+
+// } catch (e) {
+//   document.write("Error var: " + e.message);
+
+// } finally {
+//   console.log("final")
+
+//   stopLoading()
+// }
+
+// console.log("4" - 2);
+
+// let a = "Hello world";
+
+// console.log(a);
+
+// a = "Test";
+
+// console.log(a)
+
+// const greeting = "Hello";
+
+
+// if (true) {
+//   const greeting = "World";
+// }
+
+// console.log(greeting);
